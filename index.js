9bc985645f3fed7f7e6948c191111eb3cc58f3e3
edzhabs <92904122+edzhabs@users.noreply.github.com>
2022-10-08 13:50:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var gamePattern = [];
 var userPicked = [];
 var colors = ["green", "red", "yellow", "blue"];
+var sounds = {
+    green: "sounds/green.mp3",
+    red: "sounds/red.mp3",
+    blue: "sounds/blue.mp3",
+    yellow: "sounds/yellow.mp3",
+    wrong: "sounds/wrong.mp3"
+};
 var started = false;
 var level = 0;
 
@@ -18,6 +25,7 @@ $(document).keyup(function() {
 $(".btn").click(function() {
     userPicked.push(this.id);
     animatePress(this.id);
+    new Audio(sounds[this.id]).play();
     checkAnswer(userPicked.length - 1);
 });
 
@@ -47,6 +55,7 @@ function checkAnswer(currentLevel) {
     } else {
         $("body").addClass("game-over");
         $("#level-title").text("Game Over, Press Any Key to Start");
+        new Audio(sounds["wrong"]).play();
         userPicked.length = 0;
         gamePattern.length = 0;
         level = 0;
@@ -59,6 +68,8 @@ function colorSequence() {
         (function(i) {
             setTimeout(function() {
             $("#" + gamePattern[i]).delay(100).fadeOut().fadeIn("slow");
+            var sound = new Audio(sounds[gamePattern[i]]);
+            sound.play();
             }, 1000 * i);
         }(i));
     }
